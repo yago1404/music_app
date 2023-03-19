2f80c4b166c1b6dcc72c3da5d88e4f5763b20269
yago_t <50213633+yago1404@users.noreply.github.com>
2023-03-19 15:54:20 -0300

    Update profile_page.dart

--- lib/app/pages/dashboard/profile_page.dart
+++ lib/app/pages/dashboard/profile_page.dart
@@ -17,121 +17,126 @@ class _ProfilePageState extends State<ProfilePage> {
 
   _body() {
     return SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(height: 24),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+      child: Column(
+        children: [
+          const SizedBox(height: 24),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                decoration: const BoxDecoration(
+                  color: Colors.green,
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/kevin.png'),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Yago Taveiros',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 4),
+              const Text(
+                '400 Músicas',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 14,
+                ),
+              ),
+              const SizedBox(height: 12),
+            ],
+          ),
+          Expanded(
+            child: ListView(
               children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  decoration: const BoxDecoration(
-                    color: Colors.green,
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/kevin.png'),
+                const SizedBox(height: 12),
+                InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          'Perfil',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                        Icon(
+                          Icons.navigate_next,
+                          color: Colors.white,
+                        )
+                      ],
                     ),
                   ),
                 ),
-                const SizedBox(height: 16),
-                const Text(
-                  'Yago Taveiros',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
+                InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          'Privacidade',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                        Icon(
+                          Icons.navigate_next,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
                   ),
                 ),
-                const SizedBox(height: 4),
-                const Text(
-                  '400 Músicas',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 14,
+                InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          'Pagamento',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                        Icon(
+                          Icons.navigate_next,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          'Sobre',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                        Icon(
+                          Icons.navigate_next,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ],
             ),
-            const SizedBox(height: 12),
-            InkWell(
-              onTap: () {},
-              child: Padding(
-                padding: const EdgeInsets.all(24),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text(
-                      'Perfil',
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                    ),
-                    Icon(
-                      Icons.navigate_next,
-                      color: Colors.white,
-                    )
-                  ],
-                ),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: Padding(
-                padding: const EdgeInsets.all(24),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text(
-                      'Privacidade',
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                    ),
-                    Icon(
-                      Icons.navigate_next,
-                      color: Colors.white,
-                    )
-                  ],
-                ),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: Padding(
-                padding: const EdgeInsets.all(24),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text(
-                      'Pagamento',
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                    ),
-                    Icon(
-                      Icons.navigate_next,
-                      color: Colors.white,
-                    )
-                  ],
-                ),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: Padding(
-                padding: const EdgeInsets.all(24),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text(
-                      'Sobre',
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                    ),
-                    Icon(
-                      Icons.navigate_next,
-                      color: Colors.white,
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
